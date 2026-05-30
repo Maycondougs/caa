8abc5f4f131f1b0d844187cf21b184409212a073
Maycondougs <mdousilva@gmail.com>
2026-05-30 10:32:11 -0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
   return (
     <section id="home" className="pt-32 pb-20 md:pt-40 md:pb-24 px-6 overflow-hidden relative">
       <div className="max-w-7xl mx-auto">
-        <div className="geometric-card min-h-[500px] flex flex-col justify-center max-w-4xl">
+        <div className="geometric-card min-h-[500px] flex flex-col justify-center max-w-4xl mx-auto">
           <motion.div 
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
